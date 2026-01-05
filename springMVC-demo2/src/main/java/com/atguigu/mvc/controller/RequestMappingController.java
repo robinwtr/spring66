566c0723afce9e54609888ae58d86f0c2832c9a1
PbdAndreas_ <1845985019@qq.com>
2026-01-05 16:59:17 +0800

    requestmapping of  value and  method
    requestmapping of  value and  method

---
+++ springMVC-demo2/src/main/java/com/atguigu/mvc/controller/RequestMappingController.java
@@ -0,0 +1,29 @@
+package com.atguigu.mvc.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+//@RequestMapping("/hello")
+public class RequestMappingController {
+
+    @RequestMapping(
+            value = {"/testRequestMapping", "/test"},
+            method = {RequestMethod.GET, RequestMethod.POST}
+    )
+    public String success() {
+        return "success";
+    }
+    @GetMapping("/testGetMapping")
+    public String testGetMapping() {
+        return "success";
+    }
+    @RequestMapping(value = "/testPut",method=RequestMethod.PUT)
+    public String testPut(){
+        return "success";
+    }
+
+}
