969d286eb431cc1ff9ce1203371debe28b7f5cec
PbdAndreas_ <1845985019@qq.com>
2026-01-07 21:38:55 +0800

    testServletAPI
    testServletAPI

---
+++ springMVC-demo2/src/main/java/com/atguigu/mvc/controller/ParamController.java
@@ -0,0 +1,19 @@
+package com.atguigu.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ParamController {
+
+    @RequestMapping("/testServletAPI")
+    //形参位置的request表示当前请求
+    public String testServletAPI(HttpServletRequest request) {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        System.out.println("username:"+username+","+"password:"+password);
+        return "success";
+    }
+}
