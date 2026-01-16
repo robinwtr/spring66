3d479749812bbac1fc4066cfc8b3abb9ecf99532
PbdAndreas_ <1845985019@qq.com>
2026-01-16 13:42:19 +0800

    cha xun suoyou yuangong
    cha xun suoyou yuangong

--- springMVC-rest/src/main/java/com/atguigu/rest/controller/EmployeeController.java
+++ springMVC-rest/src/main/java/com/atguigu/rest/controller/EmployeeController.java
@@ -1,11 +1,25 @@
 package com.atguigu.rest.controller;
 
+import com.atguigu.rest.bean.Employee;
 import com.atguigu.rest.dao.EmployeeDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Collection;
+
+@Controller
 public class EmployeeController {
 
     @Autowired
     private EmployeeDao employeeDao;
 
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public String getAllEmployees(Model model) {
+        Collection<Employee> employeelist = employeeDao.getAll();
+        model.addAttribute("employeelist", employeelist);
+        return "employee_list";
+    }
 }
