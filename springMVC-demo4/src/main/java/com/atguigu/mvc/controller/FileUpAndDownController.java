31556d76bc13464800acf945848c02dc7af1da78
PbdAndreas_ <1845985019@qq.com>
2026-01-16 21:33:21 +0800

    FileUpAndDown
    FileUpAndDown

---
+++ springMVC-demo4/src/main/java/com/atguigu/mvc/controller/FileUpAndDownController.java
@@ -0,0 +1,47 @@
+package com.atguigu.mvc.controller;
+
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+@Controller
+public class FileUpAndDownController {
+
+    @RequestMapping("/testDown")
+    public ResponseEntity<byte[]> testResponseEntity(HttpSession session) throws Exception {
+        //获取ServletContext对象
+        ServletContext servletContext = session.getServletContext();
+        //获取服务器中文件的真实路径
+        String realPath = servletContext.getRealPath("/static/img/1.jpg");
+        System.out.println(realPath);
+        //创建输入流
+        InputStream inputStream = new FileInputStream(realPath);
+        //创建字节数组
+        byte[] bytes = new byte[inputStream.available()];
+        //将流读到字节数组中
+        inputStream.read(bytes);
+        //创建HttpHeaders对象设置响应头信息
+        MultiValueMap<String, String> headers = new HttpHeaders();
+        //设置下载方式和下载文件的名字
+        headers.add("Content-Disposition", "attachment; filename=1.jpg");
+        //设置响应状态码
+        HttpStatus status = HttpStatus.OK;
+        //创建ResponseEntity对象
+        ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(bytes, headers, status);
+        inputStream.close();
+        return responseEntity;
+    }
+
+
+}
