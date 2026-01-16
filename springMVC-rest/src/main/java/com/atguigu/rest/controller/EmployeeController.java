c1adf75171754dc4cd0b5258a09a3541fd7240b4
PbdAndreas_ <1845985019@qq.com>
2026-01-16 15:13:03 +0800

    delete
    delete

--- springMVC-rest/src/main/java/com/atguigu/rest/controller/EmployeeController.java
+++ springMVC-rest/src/main/java/com/atguigu/rest/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.atguigu.rest.dao.EmployeeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,5 +22,11 @@ public class EmployeeController {
         Collection<Employee> employeelist = employeeDao.getAll();
         model.addAttribute("employeelist", employeelist);
         return "employee_list";
+    }
+
+    @RequestMapping(value = "/employee/{id}", method = RequestMethod.DELETE)
+    public String deleteEmployee(@PathVariable("id") Integer id) {
+        employeeDao.deleteById(id);
+        return "redirect:/employee";
     }
 }
