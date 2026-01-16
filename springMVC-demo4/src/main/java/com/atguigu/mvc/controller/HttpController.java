6579119c6b30a89194f3f46558e1c9cfd025221d
PbdAndreas_ <1845985019@qq.com>
2026-01-16 19:55:32 +0800

    requestEntity
    requestEntity

--- springMVC-demo4/src/main/java/com/atguigu/mvc/controller/HttpController.java
+++ springMVC-demo4/src/main/java/com/atguigu/mvc/controller/HttpController.java
@@ -1,6 +1,7 @@
 package com.atguigu.mvc.controller;
 
 
+import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,14 @@ public class HttpController {
     @RequestMapping(value = "/testRequestBody")
     public String testRequestBody(@RequestBody String body) {
         System.out.println("body" + body);
+        return "success";
+    }
+
+    @RequestMapping("/testRequestEntity")
+    public String testRequestEntity(RequestEntity<String> requestEntity) {
+        //当前requestEntity表示整个请求报文的信息
+        System.out.println("requestEntity 请求头" + requestEntity.getHeaders());
+        System.out.println("requestEntity 请求体" + requestEntity.getBody());
         return "success";
     }
 
